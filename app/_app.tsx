5b4daa3002c639d4b18f5403640dc5211fefad11
Santhosh Tanay <tanaysunnam2003@gmail.com>
2024-11-12 16:20:15 +0530

    Update Taupo Thai website with new styling and layout

---
+++ app/_app.tsx
@@ -0,0 +1,8 @@
+// pages/_app.tsx
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: any) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Taupo Thai Restaurant',
+  description: 'Experience authentic Thai cuisine in the heart of Taupo',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
